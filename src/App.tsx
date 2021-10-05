38263b5330508f7c3781cced3b1d912563fa088c
Hans-Christian Otto <c.otto@suora.com>
2021-10-05 12:21:55 +0200

    Message View mit Optional Prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import React from "react";
+import { MessageView } from "./components/MessageView";
+
 export function App() {
-  return <h1>Hallo Leipzig!</h1>;
+  return (
+    <>
+      <h1>Hallo Leipzig!</h1>
+      <MessageView name="Florian" opinion="React ist knorke!" />
+      <MessageView opinion="React ist nicht so toll!" />
+    </>
+  );
 }
 
 export const App2 = () => {
