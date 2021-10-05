153f4b0dea04caa5129c0f71e2a3ea97e3b44a21
Hans-Christian Otto <c.otto@suora.com>
2021-10-05 13:53:20 +0200

    Cleanup

--- src/components/MessageView.tsx
+++ src/components/MessageView.tsx
@@ -1,35 +1,13 @@
 interface Props {
-  name?: string;
+  name: string;
   opinion: string;
 }
 
 export function MessageView({ opinion, name }: Props): JSX.Element {
-  // name ist entweder string oder undefined
-  if (!name) {
-    // name ist undefined oder leer
-    return (
-      <article>
-        {opinion}
-        <footer>Ein anonymer Mensch</footer>
-      </article>
-    );
-  }
-
-  // name ist definitiv ein string, weil es bei undefined vorher returned hätte
-
   return (
     <article>
       {opinion}
       <footer>{name.toUpperCase()}</footer>
-    </article>
-  );
-}
-
-export function MessageView2({ opinion, name }: Props): JSX.Element {
-  return (
-    <article>
-      {opinion}
-      {name && <footer>{name.toUpperCase()}</footer>}
     </article>
   );
 }
