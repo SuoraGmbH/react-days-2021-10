1fab9abc84016bd2db39d6e3bba6577d236b6a00
Hans-Christian Otto <c.otto@suora.com>
2021-10-05 14:06:13 +0200

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import React from "react";
 import { MessageView } from "./components/MessageView";
-import { Message } from "./domain/Message";
 
 export function App() {
-  const messageObject: Message = {
-    author: "Florian",
-    date: 24,
-    id: "42321",
-    message: "Hallo welt",
-  };
-
   return (
     <>
       <h1>Hallo Leipzig!</h1>
-      <MessageView message={messageObject} />
+      <MessageView
+        message={{
+          author: "Florian",
+          date: 24,
+          id: "42321",
+          message: "Hallo welt",
+        }}
+      />
     </>
   );
 }
