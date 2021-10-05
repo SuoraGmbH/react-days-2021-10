1a13957ddb37cddcef055de36dd543c14da0a4cd
Hans-Christian Otto <c.otto@suora.com>
2021-10-05 15:57:10 +0200

    MessageCompose state

--- src/components/MessageCompose.tsx
+++ src/components/MessageCompose.tsx
@@ -1,11 +1,25 @@
+import React, { useState } from "react";
+
 export const MessageCompose: React.FunctionComponent = () => {
+  const [inputValue, setInputValue] = useState("");
+
   const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault();
-    console.log("Abgesendet!");
+
+    if (inputValue !== "") {
+      console.log("Abgesendete Nachricht: ", inputValue);
+      setInputValue("");
+    }
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
   };
 
   return (
     <form onSubmit={handleSubmit}>
+      <input value={inputValue} onChange={handleChange} />
+      {inputValue}
       <button type="submit">Absenden</button>
     </form>
   );
