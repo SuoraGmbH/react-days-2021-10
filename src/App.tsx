068eaf0acf18ea69f8e06ca4f1397657a0459849
Hans-Christian Otto <c.otto@suora.com>
2021-10-06 11:35:22 +0200

    Moved messages to state off app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import { Counter } from "./components/Counter";
 import { GithubStats } from "./components/GithubStats";
 import { MessageCompose } from "./components/MessageCompose";
 import { MessageList } from "./components/MessageList";
 import { MessageView } from "./components/MessageView";
+import { Message } from "./domain/Message";
 
 export function App() {
+  const [messages, setMessages] = useState([
+    { author: "Florian", date: 123, id: "asd", message: "Hallooooo" },
+    { author: "Richard", date: 12423, id: "asdsadasd", message: "Hallo!!" },
+    { author: "Anni", date: 12356, id: "a312231sd", message: "Hello!" },
+  ]);
+
   return (
     <>
       <h1>Hallo Leipzig!</h1>
@@ -17,7 +25,7 @@ export function App() {
         }}
       />
       <MessageCompose />
-      <MessageList />
+      <MessageList messages={messages} />
       <hr />
       <Counter />
       <GithubStats repo="SuoraGmbH/react-days-2021-10" />
--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -1,13 +1,11 @@
 import { Message } from "../domain/Message";
 import { MessageView } from "./MessageView";
 
-export const MessageList: React.FunctionComponent = () => {
-  const messages: Message[] = [
-    { author: "Florian", date: 123, id: "asd", message: "Hallo" },
-    { author: "Richard", date: 12423, id: "asdsadasd", message: "Hallo!!" },
-    { author: "Anni", date: 12356, id: "a312231sd", message: "Hello!" },
-  ];
+interface Props {
+  messages: Message[];
+}
 
+export const MessageList: React.FunctionComponent<Props> = ({ messages }) => {
   return (
     <>
       {messages.map((message) => (
