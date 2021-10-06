aa2c985f60dd6d8d7061cbd5e2ad39a437f97eff
Hans-Christian Otto <c.otto@suora.com>
2021-10-06 11:49:52 +0200

    i cannot react

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { MessageCompose } from "./MessageCompose";
 export function Counter() {
   const [count, setCount] = useState(0);
@@ -7,7 +7,7 @@ export function Counter() {
     <div>
       <p>Count: {count}</p>
       <button onClick={() => setCount(count + 1)}>Increment</button>
-      <MessageCompose key={count} />
+      <MessageCompose key={count} onMessageSend={console.log} />
     </div>
   );
 }
