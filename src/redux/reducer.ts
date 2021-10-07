1593978326974eda0d3e7c6999b7ee6c471b7738
Hans-Christian Otto <c.otto@suora.com>
2021-10-07 10:32:03 +0200

    WIP: Reducer

--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -1,5 +1,30 @@
-export interface ApplicationState {}
+import { Message } from "../domain/Message";
 
-const initialState: ApplicationState = {};
+export interface ApplicationState {
+  messages: Message[];
+}
 
-export const reducer = (state = initialState) => state;
+const initialState: ApplicationState = {
+  messages: [],
+};
+
+const MESSAGE_ADDED = "Message/Added";
+
+interface MessageAddedAction {
+  type: string;
+  payload: Message;
+}
+
+export const reducer = (
+  state: ApplicationState = initialState,
+  action: MessageAddedAction
+): ApplicationState => {
+  if (action.type === MESSAGE_ADDED) {
+    return {
+      ...state,
+      messages: [...state.messages, action.payload],
+    };
+  }
+
+  return state;
+};
